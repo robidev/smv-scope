de074043e7699374a8e968cf5630d8b1200f5a43
Robin Massink <robin.massink@alliander.com>
2021-04-18 00:45:35 +0200

    generalised subscriber and optimised client
    Signed-off-by: Robin Massink <robin.massink@alliander.com>

--- svscope.py
+++ svscope.py
@@ -9,7 +9,56 @@ from datetime import datetime
 
 from flask import Flask, Response, render_template
 
+import gzip
+
 application = Flask(__name__)
+
+colors = ['rgb(99, 132, 255)','rgb(255, 99, 132)','rgb(132, 255, 99)','rgb(99, 99, 99)','rgb(0, 132, 255)','rgb(255, 0, 132)','rgb(132, 255, 0)','rgb(99, 99, 0)']
+
+dataConfig =  {
+                'labels': [],
+                'datasets': [],
+            }
+
+optionsConfig={
+                'animation': False,
+                'responsive': True, #for resizing
+                'title': {
+                    'display': True,
+                    'text': 'SMV 9-2 values',
+                },
+                'tooltips': {
+                    'mode': 'index',
+                    'intersect': False,
+                },
+                'hover': {
+                    'mode': 'nearest',
+                    'intersect': True,
+                },
+                'scales': {
+                    'x': {
+                        'title': { 
+                            'display': True,
+                            'text': 'smpCnt',
+                        },
+                        #//type: 'linear',#//linear causes first and last sample to connect, causing a line
+			'beginAtZero': True,
+                        #//min: 0, //seems to start at 1
+                        'max': 4000,
+                        'minRotation': 0,
+                        'maxRotation': 0,
+                    },
+                    'y': {
+                        'title': { 
+                            'display': True,
+                            'text': 'value',
+                        },
+                        'type': 'linear',
+                        'minRotation': 0,
+                        'maxRotation': 0,
+                    },
+                }
+            }
 
 
 oldSmpCnt = 0
@@ -17,12 +66,34 @@ smp = {}
 smp[0] = []
 seconds = 0
 running = False
+dataSets = 0
+
+
+def SetDataConfig(amount):
+    dataConfig['datasets'] = []
+    for idx in range(amount):
+        channel = f"Channel {idx}" 
+        dataConfig['datasets'].append( {
+                    'pointRadius': 0,
+                    'stepped': False,
+                    'tension': 0,
+                    'label': channel,
+                    'backgroundColor': colors[idx],
+                    'borderColor': colors[idx],
+                    'data': [],
+                    'fill': False,
+                    'parsing': False,
+                    'normalized': True,
+                    'borderDash': [],
+                } )
+
 
 def svUpdateListener ( subscriber, parameter,  asdu):
     global oldSmpCnt
     global smp
     global seconds
     global running
+    global dataSets
 
     #svID = lib61850.SVSubscriber_ASDU_getSvId(asdu)
     #if svID != None:
@@ -30,39 +101,36 @@ def svUpdateListener ( subscriber, parameter,  asdu):
 
     smpCnt = lib61850.SVSubscriber_ASDU_getSmpCnt(asdu)
 
+
+    size = lib61850.SVSubscriber_ASDU_getDataSize(asdu)
+
     if smpCnt != (oldSmpCnt + 1) % 4000 and running:
         #print("ERROR: smpCnt expected: %i, received: %i" % ((oldSmpCnt + 1) % 4000, smpCnt))
         # fill missing packets with null samples
         c = smpCnt
         o = oldSmpCnt + 1
         while c != o:
-            smp[seconds].append( {'x': o, 
-                'R': 0, 
-                'S': 0, 
-                'T': 0,
-                'N': 0 }
-                )
+            indices = []
+            for _ in range(0,size,8):
+                indices.append( {'y': 0 } )
+            smp[seconds].append( {'x': smpCnt, 'index': indices } )
             o = (o + 1) % 4000
 
     #print("  smpCnt: %i" % smpCnt)
     #print("  confRev: %u" % lib61850.SVSubscriber_ASDU_getConfRev(asdu))
     #print("  smpSynch: %u" % lib61850.SVSubscriber_ASDU_getSmpSynch(asdu))
-    #size = lib61850.SVSubscriber_ASDU_getDataSize(asdu)
-    #print("  size:%i" % size)
-    #for item in range(0,size,8):
-    #    print("   DATA    [%i]: %i" % (item,lib61850.SVSubscriber_ASDU_getINT32(asdu, item)))
-    #    print("   QUALITY [%i]: %i" % (item,lib61850.SVSubscriber_ASDU_getINT32(asdu, item+4)))
 
-    smp[seconds].append( {'x': smpCnt, 
-        'R': lib61850.SVSubscriber_ASDU_getINT32(asdu, 0), 
-        'S': lib61850.SVSubscriber_ASDU_getINT32(asdu, 8), 
-        'T': lib61850.SVSubscriber_ASDU_getINT32(asdu, 16),
-        'N': lib61850.SVSubscriber_ASDU_getINT32(asdu, 24)}
-        )
+    indices = []
+    #print("  size:%i" % size)
+    for item in range(0,size,8):
+        indices.append( {'y': lib61850.SVSubscriber_ASDU_getINT32(asdu, item) } )
+
+    smp[seconds].append( {'x': smpCnt, 'index': indices } )
 
     if oldSmpCnt > smpCnt: # trigger second increment when the counter loops.
         seconds = seconds + 1
         smp[seconds] = []
+        dataSets = int(size/8)
 
     oldSmpCnt = smpCnt
     running = True
@@ -79,7 +147,15 @@ def chart_data():
     def smv_data():
         while True:
             if seconds > 2:
-                json_data = json.dumps(smp[seconds-1])
+                allData = {}
+                if True:
+                    allData['dataSets'] = smp[seconds-1]
+                if True:
+                    SetDataConfig(dataSets)
+                    allData['config_data'] = dataConfig
+                    allData['config_options'] = optionsConfig
+                json_data = json.dumps(allData)
+                #json_data = json.dumps({ 'dataSets': smp[seconds-1] })
                 yield f"data:{json_data}\n\n"
             time.sleep(1)
 
